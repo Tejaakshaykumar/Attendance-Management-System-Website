0cc59662774c1cbe41cfbcea60a1ea7e2507ee1e
Tejaakshaykumar <tejaakshaykumar970@gmail.com>
2024-07-08 14:20:01 +0530

    removing unwanted folders

--- .history/login1_20240330195233.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Perform logout actions (destroy session, etc.)
-// Redirect the user to another page after logout
-header("Location: login.php"); // Redirect to login page
-exit(); // Ensure script execution stops here
-?>
